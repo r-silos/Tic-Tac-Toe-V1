ef64ee10c953b13dbf018e7c92f0ac05802304db
r-silos <r.silos36@gmail.com>
2022-07-18 12:12:21 -0500

    added some core classes to tic-tac-toe.rb

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,1 +1,25 @@
 #Lets Start out with some Pseduocode to help us plan out this project
+class Player
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+    end
+end
+
+
+class Display 
+    def initialize
+        @game_board = [['_','_','_'], ['_','_','_'], ['_','_','_']]
+    end
+
+    def display_board
+        for item in @game_board
+            print item.flatten
+            puts
+        end
+    end
+end
+
+game = Display.new
+game.display_board
